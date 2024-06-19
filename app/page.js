f58c67a3d00f9b4709dfc7b2768124544f0fa69d
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-06-20 03:48:36 +0530

    new project contact management app

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import CreateContactForm from "@/components/CreateContactForm";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="min-h-screen flex justify-center items-center bg-gray-100">
+      <h2>Welcome to CRUD with PRISMA</h2>
+     
+    </section>
+  );
+}
